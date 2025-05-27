01de16b15f12dc6e02390b126a222644427ee3ae
Kushal-Srivastav <kushalsrijan17@gmail.com>
2025-05-27 18:43:11 +0530

    Navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl underline'>
+   Welcome to my SaaS app.
+      </h1>
+    
+    <Button>
+      Let's Get Started.
+    </Button>
+    </div>
   )
 }
 
---
+++ app/subscription/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Subscription = () => {
+  return (
+    <div>
+      Subsciption
+    </div>
+  )
+}
+
+export default Subscription
