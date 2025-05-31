3de79601d93210c1b682d4d71c693a99f781b836
Kushal-Srivastav <kushalsrijan17@gmail.com>
2025-05-31 15:26:11 +0530

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,48 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>
-   Welcome to my SaaS app.
+    <main>
+      <h1>
+   Popular Companions.
       </h1>
     
-    <Button>
-      Let's Get Started.
-    </Button>
-    </div>
+    <section className='home-section'>
+       <CompanionCard
+         id="123"
+         name="Neura The Brainy Explorer."
+         topic="Neural network of the brain."
+         subject="science"
+         duration={45}
+         color="#ffda6e"
+        />
+       <CompanionCard
+        id="456"
+         name="Countsy the number wizard."
+         topic="Derivatives and Integrals."
+         subject="Maths."
+         duration={30}
+         color="#e5d0ff"
+        />
+      <CompanionCard
+       id="789"
+         name="Verba the vocabualry builder."
+         topic="Language."
+         subject="English Literature."
+         duration={30}
+         color="#BDE7FF"
+       />
+    </section>
+
+    <section className='home-section'>
+      <CompanionsList />
+      <Cta />
+    </section>
+    </main>
   )
 }
-
 export default Page
--- components/ui/Navitems.tsx
+++ components/ui/Navitems.tsx
@@ -17,7 +17,9 @@ const pathname = usePathname();
   return (
   <nav className='flex items-center gap-4'>
     {navItems.map(({label, href}) => (
-         <Link href={href} key={label} className={cn(pathname ===
+         <Link href={href} 
+         key={label} 
+         className={cn(pathname ===
             href && 'text-primary font-semibold') }
          >
             {label}
